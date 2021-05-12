4ed6b1005431697390cb2e9e05b8b6e24e20931e
David Möller <davidm@student.chalmers.se>
2021-05-12 18:57:20 +0200

    Storing leader board in sqlite database

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 require("dotenv").config({ path: __dirname + "/../.env" });
+
 const axios = require("axios");
+const sqlite3 = require("sqlite3");
+const { join } = require("path");
 const { to } = require("./util");
+
+const db = new sqlite3.Database(join(__dirname, "database"));
 
 //Cleans up the response from Strava
 const cleanLeaderBoard = resp => {
@@ -11,6 +16,7 @@ const cleanLeaderBoard = resp => {
         newBoard.push({
             name: e.athlete_firstname + " " + e.athlete_lastname,
             distance: e.distance,
+            id: e.athlete_id,
         });
     }
     newBoard.sort((a, b) => b.distance - a.distance);
@@ -52,7 +58,7 @@ const formatEntry = (space_to_d, { name, distance }) => {
 const toPost = leaderBoard => {
     const space_to_time = 20;
     let post =
-        "```Name" +
+        "Total från och med 2021-18\n```Name" +
         Array(space_to_time - 4)
             .fill("\xa0")
             .join("") +
@@ -63,20 +69,87 @@ const toPost = leaderBoard => {
     return post + "\n```";
 };
 
-// Posts the leader board in slack
-const main = async () => {
-    const board = await getLeaderBoard(process.env.club_id);
-
-    await axios.post(
-        `https://hooks.slack.com/services/${process.env.hook_token}`,
-        {
-            username: "Strava",
-            icon_emoji: ":strava:",
-            text: toPost(board),
-        },
+const createTables = db => {
+    db.run(
+        "CREATE TABLE IF NOT EXISTS users( \
+        id INT PRIMARY KEY NOT NULL, \
+        name TEXT NOT NULL\
+        )",
     );
 
-    console.log("Leader board successfully posted!");
+    db.run(
+        "CREATE TABLE IF NOT EXISTS distances( \
+        user_id INT NOT NULL, \
+        distance INT, \
+        year_week TEXT NOT NULL, \
+        FOREIGN KEY(user_id) REFERENCES users(id) \
+        )",
+    );
+};
+
+const insertUsers = (db, users) => {
+    for (var i in users) {
+        db.run(
+            "INSERT INTO users(id, name) \
+        SELECT $id, $name \
+        WHERE NOT EXISTS(SELECT 1 FROM users WHERE id = $id) \
+        ",
+            {
+                $id: users[i].id,
+                $name: users[i].name,
+            },
+        );
+    }
+};
+
+const insertDistances = (db, distances) => {
+    for (var i in distances) {
+        db.run(
+            "INSERT INTO distances(user_id, distance, year_week) \
+            SELECT $id, $distance, strftime('%Y-%W') \
+            WHERE NOT EXISTS(SELECT 1 FROM distances WHERE user_id = $id AND year_week = strftime('%Y-%W'))",
+            {
+                $id: distances[i].id,
+                $distance: distances[i].distance,
+            },
+        );
+    }
+};
+
+const postToSlack = (distances, done) => {
+    axios
+        .post(`https://hooks.slack.com/services/${process.env.hook_token}`, {
+            username: "Strava",
+            icon_emoji: ":strava:",
+            text: toPost(distances),
+        })
+        .finally(done);
+};
+
+const getTotalDistances = (db, done) =>
+    db.all(
+        "SELECT name, SUM(distance) AS distance \
+    FROM distances CROSS JOIN users ON users.id=distances.user_id \
+    GROUP BY user_id \
+    ORDER BY distance DESC",
+        done,
+    );
+
+// Posts the leader board in slack
+const main = async () => {
+    db.serialize(() => {
+        createTables(db);
+        getLeaderBoard(process.env.club_id).then(users => {
+            insertUsers(db, users);
+            insertDistances(db, users);
+            getTotalDistances(db, (_, distances) =>
+                postToSlack(distances, () => {
+                    db.close();
+                    console.log("Leader board successfully posted!");
+                }),
+            );
+        });
+    });
 };
 
 main();
